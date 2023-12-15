3832de957f9e2addf9b71b1add3ee70642668dad
Hugo Chartier <hugo.chartier1@etu.univ-nantes.fr>
2023-12-15 11:33:55 +0100

    add rapport

--- trivial_pursuit_client/packages/core/src/index.ts
+++ trivial_pursuit_client/packages/core/src/index.ts
@@ -1,4 +1,4 @@
 import "@trivial-pursuit-client/api";
 import "@trivial-pursuit-client/websocket";
 export * from './core-facade';
-// console.log("Salut de la part de Core !");
+//console.log("Salut de la part de Core !");
