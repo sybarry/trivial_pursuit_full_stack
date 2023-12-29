0c56d4a89d150ca18e736611cc624576da3b8b78
E206597M <hugo.chartier1@etu.univ-nantes.fr>
2023-12-29 17:15:23 +0100

    fix pipeline pit

---
+++ trivial_pursuit_server/trivialPursuitServer-data/src/main/java/fr/alma/trivial_pursuit_server/data/App.java
@@ -0,0 +1,13 @@
+package fr.alma.trivial_pursuit_server.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
