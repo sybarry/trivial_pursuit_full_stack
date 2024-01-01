dc49a4b69b57c2df221d7b17fb483370e2b3ef78
Hugo CHARTIER <hugo.chartier1@etu.univ-nantes.fr>
2024-01-01 22:01:10 +0000

    Add boardFactory implementation and test in core module

---
+++ trivial_pursuit_server/trivialPursuitServer-core/src/test/java/fr/alma/trivial_pursuit_server/core/cases/HeadQuarterTest.java
@@ -0,0 +1,16 @@
+package fr.alma.trivial_pursuit_server.core.cases;
+
+class HeadQuarterTest {
+    //TODO
+
+//    Patron de test
+//    @Test
+//    @DisplayName("describingText")
+//    void test(){
+//        //CONFIG
+//
+//        //ACTION
+//
+//        //VERIFY
+//    }
+}
