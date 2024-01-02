bc150ea111dc5a4413a45fb83d50588973c53cb2
Hugo Chartier <hugo.chartier1@etu.univ-nantes.fr>
2024-01-02 20:20:39 +0100

    Add card to cards.json in Core and Data module

--- trivial_pursuit_server/trivialPursuitServer-core/src/main/java/fr/alma/trivial_pursuit_server/App.java
+++ trivial_pursuit_server/trivialPursuitServer-core/src/main/java/fr/alma/trivial_pursuit_server/App.java
@@ -20,18 +20,18 @@ public class App
 //        System.out.println( "Hello World!" );
         Card card = new Card();
 
-        Question q = new Question("Le petit Lu est-il fait avec du beurre salé", null, Theme.GEOGRAPHY);
-        Question q2 = new Question("Dans quel pays habite Jessica Fletcher, le personnage de la série policière arabesque", null, Theme.ENTERTAINMENT);
-        Question q3 = new Question("A qui devont nous les chiffres que nous utilisons : aux chinois, aux arabes ou aux égyptiens", null, Theme.HISTORY);
-        Question q4 = new Question("Pour les trois mousquetaires, c'était un pour tous...", null, Theme.ARTS_LITERATURE);
-        Question q5 = new Question("Quel est le gaz le plus abondant dans l'air", null, Theme.SCIENCE_NATURE);
-        Question q6 = new Question("Quelle est d'une table de tennis de table", null, Theme.SPORTS_LEISURE);
-        Answer a = new Answer("Oui", Theme.GEOGRAPHY);
-        Answer a2 = new Answer("Aux états-unis", Theme.ENTERTAINMENT);
-        Answer a3 = new Answer("Aux arabes", Theme.HISTORY);
-        Answer a4 = new Answer("et tous pour un", Theme.ARTS_LITERATURE);
-        Answer a5 = new Answer("L'azote", Theme.SCIENCE_NATURE);
-        Answer a6 = new Answer("2,74 mètres", Theme.SPORTS_LEISURE);
+        Question q = new Question("Qu'est-ce qui est obligatoire sur la route depuis 1973 et, en ville, depuis 1984 ?", null, Theme.GEOGRAPHY);
+        Question q2 = new Question("Qui était la fiancé de Tarzan?", null, Theme.ENTERTAINMENT);
+        Question q3 = new Question("Qu'a introduit Jean Nicot en France?", null, Theme.HISTORY);
+        Question q4 = new Question("Quelle commune voit son calme troublé par les expériences du Comte ami de Spirou et Fantasio ?", null, Theme.ARTS_LITERATURE);
+        Question q5 = new Question("De quel insecte l'asticot est-il la larve ?", null, Theme.SCIENCE_NATURE);
+        Question q6 = new Question("Avec quoi joue-t-on au 421 ?", null, Theme.SPORTS_LEISURE);
+        Answer a = new Answer("Le port de la ceinture de securité", Theme.GEOGRAPHY);
+        Answer a2 = new Answer("Jane", Theme.ENTERTAINMENT);
+        Answer a3 = new Answer("Le tabac", Theme.HISTORY);
+        Answer a4 = new Answer("Champignac", Theme.ARTS_LITERATURE);
+        Answer a5 = new Answer("De la mouche", Theme.SCIENCE_NATURE);
+        Answer a6 = new Answer("Avec des dés", Theme.SPORTS_LEISURE);
 
         q.setAnswer(a);
         q2.setAnswer(a2);
