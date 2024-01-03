444af610e4f29487f1442be8db3f7e3efc9af231
Hugo CHARTIER <hugo.chartier1@etu.univ-nantes.fr>
2024-01-03 21:37:49 +0000

    Add BoardTest.java test, HeadQuarterTest.java test and SimpleCaseTest.java

--- trivial_pursuit_server/trivialPursuitServer-core/src/test/java/fr/alma/trivial_pursuit_server/core/cases/SimpleCaseTest.java
+++ trivial_pursuit_server/trivialPursuitServer-core/src/test/java/fr/alma/trivial_pursuit_server/core/cases/SimpleCaseTest.java
@@ -1,16 +1,33 @@
 package fr.alma.trivial_pursuit_server.core.cases;
 
-class SimpleCaseTest {
-    //TODO
+import fr.alma.trivial_pursuit_server.util.Color;
+import fr.alma.trivial_pursuit_server.util.Theme;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-//    Patron de test
-//    @Test
-//    @DisplayName("describingText")
-//    void test(){
-//        //CONFIG
-//
-//        //ACTION
-//
-//        //VERIFY
-//    }
+import java.util.Arrays;
+
+class SimpleCaseTest {
+
+    private SimpleCase simpleCase;
+
+    @BeforeEach
+    void setUp(){
+        simpleCase = new SimpleCase("simpleCase", Color.PURPLE, Arrays.asList("case1", "case2"), Theme.ARTS_LITERATURE);
+    }
+
+
+    @Test
+    @DisplayName("test parametrized constructor")
+    void testParametrizedConstructor(){
+        //CONFIG
+        //ACTION
+        //VERIFY
+        Assertions.assertEquals(Color.PURPLE, simpleCase.getColor());
+        Assertions.assertEquals("simpleCase", simpleCase.getName());
+        Assertions.assertEquals(Arrays.asList("case1", "case2"), simpleCase.getNeighbors());
+        Assertions.assertEquals(Theme.ARTS_LITERATURE, simpleCase.getTheme());
+    }
 }
