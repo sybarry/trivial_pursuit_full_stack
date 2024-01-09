ee11fc15c72f47456dc7fc7fdba356e819207f67
E206597M <hugo.chartier1@etu.univ-nantes.fr>
2024-01-09 08:35:49 +0100

    Refix index for test report

---
+++ trivial_pursuit_server/trivialPursuitServer-api/src/main/java/fr/alma/trivial_pursuit_server/App.java
@@ -0,0 +1,12 @@
+package fr.alma.TrivialPursuitServer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args ) {
+        /*System.out.println( "Hello World!" );*/}
+}
+
